90a5a192dd0285842a7331337041cf7f2e02b456
OrbitalCardinal <raulcepedac1@gmail.com>
2021-10-16 14:20:27 -0500

    Sign Up fixed

--- litmath/lib/providers/user_provider.dart
+++ litmath/lib/providers/user_provider.dart
@@ -6,26 +6,11 @@ import 'package:litmath/models/user.dart';
 import 'package:http/http.dart' as http;
 import 'package:flutter/services.dart';
 
-class UserProvider with ChangeNotifier{
-
-  String _token = '';
-  String _userId ='';
-
-  bool get isAuth {
-    return _token != null;
-  }
-
-  String get userId {
-    return _userId;
-  }
-  
+class UserProvider {
   //-------------------SIGNUP-------------------------------------------------------------------------
   Future<void> signup(String email, String password) async {
-
     const url =
         'https://identitytoolkit.googleapis.com/v1/accounts:signUp?key=AIzaSyBcrQcJoGYKHlIUwdAFDdYqlSmhjpQgBGc';
-    const verifyUrl =
-        'https://identitytoolkit.googleapis.com/v1/accounts:sendOobCode?key=AIzaSyBcrQcJoGYKHlIUwdAFDdYqlSmhjpQgBGc';
 
     final response = await http.post(
       Uri.parse(url),
@@ -35,31 +20,12 @@ class UserProvider with ChangeNotifier{
           'password': password,
           'returnSecureToken': true,
         },
-      ),  
+      ),
     );
 
     String uuid = json.decode(response.body)["localId"];
-    
-    notifyListeners();
     if (response.statusCode >= 400) {
       throw HttpException('Hubo un problema al intentar registrarse');
-    } else {
-      final emailResponse = await http.post(Uri.parse(verifyUrl),
-          body: json.encode({
-            'requestType': "VERIFY_EMAIL",
-            'idToken': json.decode(response.body)['idToken'],
-          }));
-      if(emailResponse.statusCode >= 400) {
-        throw HttpException('Hubo un problema al intentar enviar el correo de verficación');
-      } else {
-        String dbURL = "https://litmath-default-rtdb.firebaseio.com/users.json";
-        final userTypeResponse = await http.post(Uri.parse(dbURL), body: json.encode(
-          {
-            "userId": uuid,
-          }
-        ));
-        print(userTypeResponse.statusCode);
-      }
     }
   }
 }
--- litmath/lib/screens/signup_screen.dart
+++ litmath/lib/screens/signup_screen.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:litmath/providers/user_provider.dart';
+import 'package:litmath/screens/login_screen.dart';
 import 'package:litmath/utilities/constants.dart';
 import 'package:provider/provider.dart';
 
@@ -9,15 +10,13 @@ class SignUpScreen extends StatefulWidget {
 
   @override
   _SignUpScreenState createState() => _SignUpScreenState();
-
 }
 
 class _SignUpScreenState extends State<SignUpScreen> {
-
   final nameController = TextEditingController();
   final emailController = TextEditingController();
   final passwordController = TextEditingController();
-  
+
   @override
   void dispose() {
     // Clean up the controller when the widget is disposed.
@@ -27,159 +26,167 @@ class _SignUpScreenState extends State<SignUpScreen> {
     super.dispose();
   }
 
-  Future<void> _submit(BuildContext context, String email, String password ) async {
-
-        try {
-          await Provider.of<UserProvider>(context, listen: false)
-              .signup(email, password);
-        } catch (error) {
-          showDialog(
-              context: context,
-              builder: (_) => AlertDialog(
-                title: Text('Error de solicitud HTTP'),
+  Future<void> _submit(
+      BuildContext context, String email, String password) async {
+    try {
+      UserProvider().signup(email, password);
+    } catch (error) {
+      showDialog(
+          context: context,
+          builder: (_) => AlertDialog(
+                title: const Text('Error de solicitud HTTP'),
                 content: Text(error.toString()),
               ));
-          return;
-        }
-        showDialog(
-            context: context,
-            builder: (_) => AlertDialog(
-              content: Text('Se envió un correo de verificación'),
+      return;
+    }
+    showDialog(
+        context: context,
+        builder: (_) => AlertDialog(
+              content: const Text('Se registró correctamente'),
               actions: [
                 TextButton(
                   onPressed: () => Navigator.of(context).pop(),
-                  child: Text('Okay'),
+                  child: const Text('Okay'),
                 ),
               ],
             )).then((_) {
-          //Navigator.of(context).pushReplacementNamed(SlidesScreen.routeName);
-        });
+      Navigator.of(context).pushReplacementNamed(LoginScreen.routeName);
+    });
   }
 
-  Widget _NombreTextField(){
+  Widget _NombreTextField() {
     return Column(
-                      crossAxisAlignment: CrossAxisAlignment.start,
-                      children: <Widget>[
-                        Text(
-                          'Nombre',
-                          style: kLabelStyle,
-                        ),
-                        SizedBox(height: 10.0),
-                        Container(
-                          alignment: Alignment.centerLeft,
-                          decoration: kBoxDecorationStyle,
-                          height: 60.0,
-                          child: TextField(controller: nameController, keyboardType: TextInputType.emailAddress,
-                          style: TextStyle(color: Colors.black),
-                          decoration: InputDecoration(
-                            border: InputBorder.none,
-                            contentPadding: EdgeInsets.only(top: 14.0),
-                            prefixIcon: Icon(
-                              Icons.person,
-                              color:Colors.black,
-                            ),
-                            hintText: 'Ingrese el nombre de su hijo',
-                            hintStyle: kHintTextStyle,
-                          ),
-                        ),
-                      ),
-                    ],
-              );
+      crossAxisAlignment: CrossAxisAlignment.start,
+      children: <Widget>[
+        Text(
+          'Nombre',
+          style: kLabelStyle,
+        ),
+        const SizedBox(height: 10.0),
+        Container(
+          alignment: Alignment.centerLeft,
+          decoration: kBoxDecorationStyle,
+          height: 60.0,
+          child: TextField(
+            controller: nameController,
+            keyboardType: TextInputType.emailAddress,
+            style: const TextStyle(color: Colors.black),
+            decoration: InputDecoration(
+              border: InputBorder.none,
+              contentPadding: const EdgeInsets.only(top: 14.0),
+              prefixIcon: const Icon(
+                Icons.person,
+                color: Colors.black,
+              ),
+              hintText: 'Ingrese el nombre de su hijo',
+              hintStyle: kHintTextStyle,
+            ),
+          ),
+        ),
+      ],
+    );
   }
 
-  Widget _EmailTextField(){
+  Widget _EmailTextField() {
     return Column(
-                      crossAxisAlignment: CrossAxisAlignment.start,
-                      children: <Widget>[
-                        Text(
-                          'Email',
-                          style: kLabelStyle,
-                        ),
-                        SizedBox(height: 10.0),
-                        Container(
-                          alignment: Alignment.centerLeft,
-                          decoration: kBoxDecorationStyle,
-                          height: 60.0,
-                          child: TextField(controller: emailController, keyboardType: TextInputType.name,
-                          style: TextStyle(color: Colors.black),
-                          decoration: InputDecoration(
-                            border: InputBorder.none,
-                            contentPadding: EdgeInsets.only(top: 14.0),
-                            prefixIcon: Icon(
-                              Icons.email,
-                              color:Colors.black,
-                            ),
-                            hintText: 'Ingrese su email',
-                            hintStyle: kHintTextStyle,
-                          ),
-                        ),
-                      ),
-                    ],
-              );
+      crossAxisAlignment: CrossAxisAlignment.start,
+      children: <Widget>[
+        Text(
+          'Email',
+          style: kLabelStyle,
+        ),
+        SizedBox(height: 10.0),
+        Container(
+          alignment: Alignment.centerLeft,
+          decoration: kBoxDecorationStyle,
+          height: 60.0,
+          child: TextField(
+            controller: emailController,
+            keyboardType: TextInputType.name,
+            style: TextStyle(color: Colors.black),
+            decoration: InputDecoration(
+              border: InputBorder.none,
+              contentPadding: EdgeInsets.only(top: 14.0),
+              prefixIcon: Icon(
+                Icons.email,
+                color: Colors.black,
+              ),
+              hintText: 'Ingrese su email',
+              hintStyle: kHintTextStyle,
+            ),
+          ),
+        ),
+      ],
+    );
   }
 
-  Widget _PasswordTextField(){
+  Widget _PasswordTextField() {
     return Column(
-                      crossAxisAlignment: CrossAxisAlignment.start,
-                      children: <Widget>[
-                        Text(
-                          'Password',
-                          style: kLabelStyle,
-                        ),
-                        SizedBox(height: 10.0),
-                        Container(
-                          alignment: Alignment.centerLeft,
-                          decoration: kBoxDecorationStyle,
-                          height: 60.0,
-                          child: TextField(controller: passwordController, obscureText: true,
-                          style: TextStyle(color: Colors.black),
-                          decoration: InputDecoration(
-                            border: InputBorder.none,
-                            contentPadding: EdgeInsets.only(top: 14.0),
-                            prefixIcon: Icon(
-                              Icons.lock,
-                              color:Colors.black,
-                            ),
-                            hintText: 'Ingrese una contraseña',
-                            hintStyle: kHintTextStyle,
-                          ),
-                        ),
-                      ),
-                    ],
-              );
+      crossAxisAlignment: CrossAxisAlignment.start,
+      children: <Widget>[
+        Text(
+          'Password',
+          style: kLabelStyle,
+        ),
+        SizedBox(height: 10.0),
+        Container(
+          alignment: Alignment.centerLeft,
+          decoration: kBoxDecorationStyle,
+          height: 60.0,
+          child: TextField(
+            controller: passwordController,
+            obscureText: true,
+            style: TextStyle(color: Colors.black),
+            decoration: InputDecoration(
+              border: InputBorder.none,
+              contentPadding: EdgeInsets.only(top: 14.0),
+              prefixIcon: Icon(
+                Icons.lock,
+                color: Colors.black,
+              ),
+              hintText: 'Ingrese una contraseña',
+              hintStyle: kHintTextStyle,
+            ),
+          ),
+        ),
+      ],
+    );
   }
 
   Widget _CreateAccount() {
     return Container(
       alignment: Alignment.centerRight,
-      child:TextButton(style: TextButton.styleFrom(textStyle: const TextStyle(fontSize: 15),
-      ),
+      child: TextButton(
+        style: TextButton.styleFrom(
+          textStyle: const TextStyle(fontSize: 15),
+        ),
         onPressed: () => print('Crear una cuenta pressed'),
-          child: const Text('Crear una cuenta'),
-           ),
-          );
+        child: const Text('Crear una cuenta'),
+      ),
+    );
   }
-  
+
   Widget _RegisterButton(BuildContext context, String email, String password) {
-    
-  return Container(
-                      padding: EdgeInsets.symmetric(vertical: 25.0),
-                      width: double.infinity,
-                      child:ElevatedButton(
-                        style: ElevatedButton.styleFrom(
-                          elevation: 5.0,
-                          padding: EdgeInsets.all(15.0),
-                          shape: RoundedRectangleBorder(
-                            borderRadius: BorderRadius.circular(30.0),
-                            ),
-                            primary: Colors.lightBlue,),
-                            child: Text(
-                              'CREAR UNA CUENTA',
-                              style: TextStyle(
-                                letterSpacing: 1.5
-                              ),
-                            ),
-                        onPressed: () => {_submit(context, emailController.text, passwordController.text)}
+    return Container(
+        padding: EdgeInsets.symmetric(vertical: 25.0),
+        width: double.infinity,
+        child: ElevatedButton(
+            style: ElevatedButton.styleFrom(
+              elevation: 5.0,
+              padding: EdgeInsets.all(15.0),
+              shape: RoundedRectangleBorder(
+                borderRadius: BorderRadius.circular(30.0),
+              ),
+              primary: Colors.lightBlue,
+            ),
+            child: Text(
+              'CREAR UNA CUENTA',
+              style: TextStyle(letterSpacing: 1.5),
+            ),
+            onPressed: () => {
+                  _submit(
+                      context, emailController.text, passwordController.text)
+                }
 /*                         {showDialog(
                           context: context, 
                           builder: (context){
@@ -188,19 +195,18 @@ class _SignUpScreenState extends State<SignUpScreen> {
                               nameController.text + '\n' + passwordController.text),
                             );
                           })}, */
-                      )
-                    );
-}
+            ));
+  }
 
-  @override 
+  @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body:Stack(
+      body: Stack(
         children: <Widget>[
           Container(
             height: double.infinity,
             width: double.infinity,
-           /*  decoration: BoxDecoration(
+            /*  decoration: BoxDecoration(
               gradient: LinearGradient(
                 begin: Alignment.topCenter,
                 end: Alignment.bottomCenter,
@@ -221,32 +227,33 @@ class _SignUpScreenState extends State<SignUpScreen> {
               padding: EdgeInsets.symmetric(
                 horizontal: 40.0,
                 vertical: 120.0,
-                ),
-                child: Column(
-                  mainAxisAlignment: MainAxisAlignment.center,
-                  children: <Widget>[
-                    Text(
-                      'Crear una cuenta',
-                      style: TextStyle(
-                        color: Colors.black,
-                        fontFamily: 'OpenSans',
-                        fontSize: 30.0,
-                        fontWeight: FontWeight.bold,
-                      ),
+              ),
+              child: Column(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: <Widget>[
+                  Text(
+                    'Crear una cuenta',
+                    style: TextStyle(
+                      color: Colors.black,
+                      fontFamily: 'OpenSans',
+                      fontSize: 30.0,
+                      fontWeight: FontWeight.bold,
                     ),
-                    SizedBox(height: 30.0),
-                    _NombreTextField(),
-                    SizedBox(height: 30.0),
-                    _EmailTextField(),
-                    SizedBox(height: 30.0),
-                    _PasswordTextField(),
-                    _RegisterButton(context, emailController.text,passwordController.text),
-                  ],
-                ),
-          ),
+                  ),
+                  SizedBox(height: 30.0),
+                  _NombreTextField(),
+                  SizedBox(height: 30.0),
+                  _EmailTextField(),
+                  SizedBox(height: 30.0),
+                  _PasswordTextField(),
+                  _RegisterButton(
+                      context, emailController.text, passwordController.text),
+                ],
+              ),
+            ),
           ),
         ],
-      ) ,
+      ),
     );
   }
 }
