070064f15d338e454e99c5efd415ab6a9bc4a1aa
AlejandroVillarreal <alexvodark@hotmail.com>
2021-10-15 19:21:20 -0500

    user model created

---
+++ litmath/lib/models/user.dart
@@ -0,0 +1,12 @@
+class User {
+  String name = '';
+  String email = '';
+  String password = '';
+
+  User(String name, String email, String password) {
+    this.name = name;
+    this.email = email;
+    this.password = password;
+  }
+  
+}
